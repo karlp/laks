fb2f129c8578363b46df02eb1224c624f6f10ef8
Karl Palsson <karl.palsson@marel.com>
2024-02-07 15:27:08 +0000

    platform_spec: case insensitive !re please
    Given that the primary consumer of this is providing device part names,
    do it in a case insensitive manner out of the box.

--- build/scons_tools/tool_platform_spec.py
+++ build/scons_tools/tool_platform_spec.py
@@ -15,7 +15,7 @@ class ReMatcher(Matcher):
         self.pattern = pattern
     
     def match(self, meta, key):
-        m = re.match(self.pattern, meta.get(key, ''))
+        m = re.match(self.pattern, meta.get(key, ''), flags=re.IGNORECASE)
 
         if m:
             meta.update(m.groupdict())
