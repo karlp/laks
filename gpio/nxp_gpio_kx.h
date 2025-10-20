4ae0056ed3f8ce5d354b58279d644dbc155319ca
Karl Palsson <karl.palsson@marel.com>
2025-10-20 13:12:43 +0000

    kinetis: expand PCRx helpers
    It's still way too awkward to tie a gpio and a pin together, but this is
    an improvement...
    
    Pin lol = GPIOB[4]
    PinMux lol_mux = PCRB[lol.n];
    
    lol_mux.mux()
    lol.blah()

--- gpio/nxp_gpio_kx.h
+++ gpio/nxp_gpio_kx.h
@@ -20,10 +20,10 @@ struct NXP_GPIO_KX_reg_t {
 };
 
 // FIXME - unhappy with this, too awkward to use
-template <typename T>
-class NXP_PCR_KX_t : public mmio_ptr<T> {
+// Ideally I want to link the PCR from the GPIO...
+class NXP_PCR_KX_t : public mmio_ptr<NXP_PCR_KX_reg_t> {
 	public:
-		using mmio_ptr<T>::ptr;
+		using mmio_ptr<NXP_PCR_KX_reg_t>::ptr;
 		enum MuxMode {
 			Disabled = 0,
 			Alt1_GPIO = 1,
@@ -35,20 +35,39 @@ class NXP_PCR_KX_t : public mmio_ptr<T> {
 			Alt7,
 		};
 
-		void mux(uint32_t pin, MuxMode mode) const {
-			ptr()->PCR[pin] = (ptr()->PCR[pin] & ~(0x7<<8)) | (mode & 0x7) << 8;
+		class PinMux {
+			private:
+				const NXP_PCR_KX_t& p;
+
+			public:
+				const int n;
+				constexpr PinMux(const NXP_PCR_KX_t& p, const int pin) : p(p), n(pin) {}
+
+				void mux(MuxMode mode) {
+					p->PCR[n] = (p->PCR[n] & ~(0x7<<8)) | (mode & 0x7) << 8;
+				}
+
+				// Could also have an enum for up/down/off?
+				void pull(bool enable, bool up) {
+					p->PCR[n] = (p->PCR[n] & ~(3)) | (enable ? 2 : 0) | (up ? 1 : 0);
+				}
+		};
+
+		constexpr PinMux operator[](int pin) const {
+			return PinMux(*this, pin);
 		}
+
 };
 
 
 class NXP_GPIO_KX_t : public mmio_ptr<NXP_GPIO_KX_reg_t> {
 	public:
 		using mmio_ptr<NXP_GPIO_KX_reg_t>::ptr;
-		
+
 		class Pin {
 			private:
 				const NXP_GPIO_KX_t& g;
-			
+
 			public:
 				const int n;
 				constexpr Pin(const NXP_GPIO_KX_t& gpio, const int pin) : g(gpio), n(pin) {}
@@ -59,15 +78,15 @@ class NXP_GPIO_KX_t : public mmio_ptr<NXP_GPIO_KX_reg_t> {
 				void set_in() {
 					g->PDDR &= ~(1<<n);
 				}
-				
+
 				void on() {
 					g->PSOR = 1 << n;
 				}
-				
+
 				void off() {
 					g->PCOR = 1 << n;
 				}
-				
+
 				void set(bool value) {
 					if(value) {
 						on();
@@ -75,11 +94,11 @@ class NXP_GPIO_KX_t : public mmio_ptr<NXP_GPIO_KX_reg_t> {
 						off();
 					}
 				}
-				
+
 				bool get() {
 					return g->PDIR & (1 << n);
 				}
-				
+
 				void toggle() {
 					g->PTOR = (1<<n);
 				}
@@ -91,35 +110,35 @@ class NXP_GPIO_KX_t : public mmio_ptr<NXP_GPIO_KX_reg_t> {
 				const NXP_GPIO_KX_t& g;
 				int f;
 				int l;
-				
+
 				// constexpr uint32_t mask1() {
 				// 	return ((2 << l) - 1) ^ ((1 << f) - 1);
 				// }
-				
+
 				// constexpr uint32_t mask2() {
 				// 	return ((4 << (l * 2)) - 1) ^ ((1 << (f * 2)) - 1);
 				// }
-			
+
 			public:
 				constexpr PinArray(const NXP_GPIO_KX_t& gpio, int first, int last) : g(gpio), f(first), l(last) {}
-				
+
 				// void set(uint16_t value) {
 				// 	value <<= f;
 				// 	g->BSRR = ((~value & mask1()) << 16) | (value & mask1());
 				// }
-				
+
 				// uint16_t get() {
 				// 	return (g->IDR & mask1()) >> f;
 				// }
 		};
-		
+
 		constexpr Pin operator[](int pin) const {
 			return Pin(*this, pin);
 		}
-		
+
 		constexpr PinArray array(int first, int last) const {
 			return PinArray(*this, first, last);
 		}
-		
+
 };
 
